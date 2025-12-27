bdd785407a94af2fadb3f688427cff342315411c
NGUYỄN TRẦN PHÚ QUÝ <nguyentranphuquy@gmail.com>
2025-12-27 22:46:33 +0700

    demo bad commit

--- public/app.js
+++ public/app.js
@@ -1,7 +1,11 @@
 
 let count = 0;
 
-function increment() {
+this is a bad syntax error and should be detected by the CI/CD pipeline
+
+lo0lololololol {}
+
+function increment() {{}
     count = count + 1;
     const displayElement = document.getElementById('counter-value');
     displayElement.innerText = count;
