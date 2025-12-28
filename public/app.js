806f5453c4c2587f90e3f55a24be3da4d2681577
NGUYỄN TRẦN PHÚ QUÝ <nguyentranphuquy@gmail.com>
2025-12-28 16:44:29 +0700

    fix bad commit

--- public/app.js
+++ public/app.js
@@ -7,8 +7,6 @@ function increment() {
     const displayElement = document.getElementById('counter-value');
     displayElement.innerText = count;
     console.log("Counter updated to: " + count);
-    console.logggggg("abc");
-    y   eherge wgergergre asfafwafwa 
 }
 
 window.increment = increment;
